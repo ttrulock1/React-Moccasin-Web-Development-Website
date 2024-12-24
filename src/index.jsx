292cb6a8a3161957a6305679b28cb5af25e51df3
Todd Trulock <toddstrulock@gmail.com>
2024-12-24 18:37:26 -0500

    all changes.
    Signed-off-by: Todd Trulock <toddstrulock@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,40 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Ensure this path is correct
 
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
-import Services from "./pages/Services.jsx";
-import Links from "./pages/Links.jsx";
-import Contact from "./pages/Contact.jsx";
-import Header from "./components/Header.jsx";
-import Footer from "./components/Footer.jsx";
-import Websites from "./pages/Websites.jsx";
-import SocialMedia from "./pages/SociaMedia.jsx";
-import Testimonials from "./pages/Testimonials.jsx";
-
-import "./assets/styles/style.css";
-import "./assets/styles/style2.css";
-
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/links" element={<Links />} />
-        <Route path="/contactUs" element={<Contact />} />
-        <Route path="/websites" element={<Websites />} />
-        <Route path="/socialmedia" element={<SocialMedia />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-}
-
-export default App;
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
